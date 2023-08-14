b2f3f906cf727a85d360d41418fc2b00b8540d27
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-08-14 14:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-//your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-touristSpots.sort();
+let obj = {}
+
+let arr=[]
+
+for(let i=0; i<touristSpots.length; i++){
+	let word = touristSpots[i].split(" ")
+	let str=""
+	for(let j=0; j<word.length; j++){
+		let curr = word[j].toLowerCase()
+		if(curr!=="a"&& curr!=="the"&& curr!=="an"){
+			str+=word[j]+ " ";
+		}
+	}
+	let strFinal = str.trim();
+	obj[strFinal] = touristSpots[i];
+    arr.push(obj[strFinal]);
+}
+console.log(arr.sort())
+
+
